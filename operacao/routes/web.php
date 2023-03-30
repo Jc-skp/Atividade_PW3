352ee30768c4070e2b00bb14eccf0c9b9d25c84d
Jc-skp <113411979+Jc-skp@users.noreply.github.com>
2023-03-30 18:55:35 -0300

    Update web.php

--- operacao/routes/web.php
+++ operacao/routes/web.php
@@ -16,19 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello/{name}', function ($name) {
-    return view('hello', ['name' => $name]);
-})->where('name', '^[A-Z][a-z]{2,}$'); // Aceita apenas letras, sendo a primeira maiúscula, e contendo mais de duas letras.
-Route::get('/conta/{number1}/{number2}/{operation?}', function ($number1, $number2, $operation = null) {
-    return view('conta', ['number1'=>$number1, 'number2'=>$number2, 'operation'=>$operation]);
+Route::get('/diga seu nome/{name}', function ($name) {
+     view('Olá', ['name' => $name]);
+})->where('name', '[a-z]{2,}$'); 
+Route::get('/operacao/{number1}/{number2}/{operation?}', function ($number1, $number2, $operation = 'todas') {
+    switch($operation){
+        case $operation == 'todas';
+            echo 
+                '<p>Soma = '. $number1 + $number2 . 
+                '<br>Subtração = '. $number1 - $number2 . 
+                '<br>Multiplicação = '. $number1 * $number2 . 
+                '<br>Divisão = '. $number1 / $number2;
+            break;
+        case $operation == 'Soma';
+            echo '<p>Soma = '. $number1 + $number2 . '<p>';
+            break;
+        case $operation == 'Subtração';
+            echo '<p>Subtração = '. $number1 - $number2 . '<p>';
+            break;
+        case $operation == 'Multiplicação';
+            echo '<p> Multiplicação = '. $number1 * $number2 . '<p>';
+            break;
+        case $operation == 'Divisão';
+            echo '<p>Divisão = '. $number1 / $number2 . '<p>';
+            break;
+    }
+    return view('operacao', ['number1'=>$number1, 'number2'=>$number2, 'operation'=>$operation]);
 })->where('number1', '^[1-9][0-9]?$')
-    ->where('number2', '^[1-9][0-9]?$'); // Aceita números, que não podem começar com 0, opcional a quantidade de dígitos numéricos.
+    ->where('number2', '^[1-9][0-9]?$');
 
-Route::get('/idade/{year}/{month?}/{day?}', function ($year, $month = null, $day = null) {
-    $actualDate = date("d-m-Y");
-    return view('idade', ['year'=>$year, 'month'=>$month, 'day'=>$day, 'actualDate'=>$actualDate]);
-    {{list($actualDay, $actualMonth, $actualYear) = explode("-", $actualDate);}}
-    return view('idade', ['year'=>$year, 'month'=>$month, 'day'=>$day, 'actualYear'=>$actualYear, 'actualMonth'=>$actualMonth, 'actualDay'=>$actualDay]);
-})->where('year', '[0-9]{4}')
-    ->where('month', '[0-9]{1,2}?')
-        ->where('day', '[0-9]{1,2}?');
